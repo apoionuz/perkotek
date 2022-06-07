db842efa218e6ed603590a52b24a58dc5df6b3bf
apoionuz <42186592+apoionuz@users.noreply.github.com>
2022-06-07 20:56:12 +0300

    Update perkotek.py

--- perkotek.py
+++ perkotek.py
@@ -6,41 +6,37 @@ from pyairtable import Table
 from datetime import datetime
 
 # Excel Dosyalarının Okunması
-giris_cikis = pd.read_excel("<PERKOTEK_EXCEL_ÇIKTISI_ADRESİ>", index_col=None)
+data = pd.read_excel("<EXCEL DOSYASI ADRESİ>", index_col = None)
 
-# Tarihe göre filtre yapılması
-data1 = giris_cikis[giris_cikis["tarih"] == "2021-03-01"]
-
-# data1'de Index'in sıfırlanması
-data1.reset_index(drop=True, inplace=True)
+## Tarihe göre filtre yapılması
+# data = giris_cikis[giris_cikis["tarih"] == "2021-03-01"]
+# data.reset_index(drop=True, inplace=True)
 
 # Airtable Bağlantısı
 api_key = "<API KEY>"
-table = Table(api_key, '<base_id>', '<table_id>')
+table = Table(api_key, '<BASE ID>', '<TABLO ID>')
 
 # Giriş tarihi ve saatinin kombine edilmesi
-for i in range(len(data1)):
-    if type(data1.loc[i, 'giris_saat']) != float:
-        tarih = pd.Timestamp(data1.loc[i, 'tarih']).to_pydatetime() # "1999-08-07" verisinin datetime formatına dönüştürülmesi
-        giris_saati = data1.loc[i, 'giris_saat'] 
-        giris_tarihi = datetime.combine(tarih, giris_saati) # iki datetime verisinin kombine edilmesi
-        data1.loc[i,"giris"] = giris_tarihi # kombine edilen verinin yeni bir sütuna atanması
-
-# Çıkış tarihi ve saatinin kombine edilmesi
-for i in range(len(data1)):
-    if type(data1.loc[i, 'cikis_saat']) != float:
-        tarih = pd.Timestamp(data1.loc[i, 'tarih']).to_pydatetime() 
-        cikis_saati = data1.loc[i, 'cikis_saat']
+for i in range(len(data)):
+    if type(data.loc[i, 'giris_saat']) != float and type(data.loc[i, 'cikis_saat']) != float:
+        # Tarih ve saat sütunlarının datetime formatına dönüştürülmesi
+        tarih = pd.Timestamp(data.loc[i, 'tarih']).to_pydatetime()  
+        giris_saati = data.loc[i, 'giris_saat']                     
+        cikis_saati = data.loc[i, 'cikis_saat']
+        # Tarih ve saat sütunlarının kombine edilmesi
+        giris_tarihi = datetime.combine(tarih, giris_saati) 
         cikis_tarihi = datetime.combine(tarih, cikis_saati)
-        data1.loc[i,"cikis"] = cikis_tarihi
+        # Kombine edilen yeni tarihlerin yeni bir sütuna atanması
+        data.loc[i,"giris"] = giris_tarihi 
+        data.loc[i,"cikis"] = cikis_tarihi       
+        # Giriş ve çıkış tarihlerinin Airtable'ın okuyabileceği bir formata getirilmesi
+        data.loc[i, "giris_airtable"] = str(data.loc[i,"giris"]).replace(" ","T")+".000Z"
+        data.loc[i, "cikis_airtable"] = str(data.loc[i,"cikis"]).replace(" ","T")+".000Z"
 
-# Giriş ve çıkış tarihlerinin Airtable'ın okuyabileceği bir formata getirilmesi
-for i in range(len(data1)):
-    data1.loc[i, "giris_airtable"] = str(data1.loc[i,"giris"]).replace(" ","T")+".000Z"
-    data1.loc[i, "cikis_airtable"] = str(data1.loc[i,"cikis"]).replace(" ","T")+".000Z"
 
 # Bilgilerin Airtable'a aktarılması
-for i in range(len(data1)):   
-    table.create({'Name': str(data1.loc[i,"personel_id"]) ,
-                    "Giriş Saati" : str(data1.loc[i,"giris_airtable"]),
-                    "Çıkış Saati" : str(data1.loc[i,"cikis_airtable"])})
+# Airtable tarih sütunlarında "Include Time" açık olmalı ve "Use the same time zone (GMT) for all collaborators" seçilmelidir.
+for i in range(len(data)):   
+    table.create({'Name': str(data.loc[i,"personel_id"]) ,
+                    "Giriş Saati" : str(data.loc[i,"giris_airtable"]),   
+                    "Çıkış Saati" : str(data.loc[i,"cikis_airtable"])})
